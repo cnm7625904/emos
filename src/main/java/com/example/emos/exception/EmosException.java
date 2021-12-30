59b83341031ed857cde0461770b1566f0a39e59b
451083622 <451083622@qq.com>
2021-12-30 13:15:26 +0800

    修改包名

---
+++ src/main/java/com/example/emos/exception/EmosException.java
@@ -0,0 +1,15 @@
+package com.example.emos.exception;
+
+/**
+ * copyright (C), 2021, 运达科技有限公司
+ * fileName  EmosException
+ *
+ * @author 王玺权
+ * date  2021-12-30 13:14
+ * description 异常类
+ * reviser
+ * revisionTime
+ */
+public class EmosException extends RuntimeException{
+
+}
