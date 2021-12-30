32215873894b4d5dc4c3f69901241fd2bb2ab29c
451083622 <451083622@qq.com>
2021-12-30 13:08:51 +0800

    Initial commit

---
+++ src/main/java/com/example/emos/ex/exception.java
@@ -0,0 +1,14 @@
+package com.example.emos.ex;
+
+/**
+ * copyright (C), 2021, 运达科技有限公司
+ * fileName  exception
+ *
+ * @author 王玺权
+ * date  2021-12-30 13:07
+ * description 异常类
+ * reviser
+ * revisionTime
+ */
+public class exception extends RuntimeException{
+}
